08bf5e51fbb93e7680153768a69905bdfcea820d
AnnaSamonenko <annsamonenko97@gmail.com>
2018-10-10 17:52:40 +0300

    Added scripts for displaying table

--- src/main/resources/display/displayWorkingHistoryTable.sql
+++ src/main/resources/display/displayWorkingHistoryTable.sql
@@ -1,8 +1,8 @@
-SELECT e.name, pr.title, wh.start_date, wh.end_date, p.title
+SELECT e.name, pr.title AS project_title, wh.start_date, wh.end_date, p.title as position_title
 FROM working_history AS wh
 JOIN employees AS e
 ON e.employee_id =  wh.employee_id
 JOIN projects AS pr
 ON pr.project_id = wh.project_id
 JOIN positions AS p
-ON p.position_id = e.position_id AND p.title = 'Test Engineer';
+ON p.position_id = e.position_id;
