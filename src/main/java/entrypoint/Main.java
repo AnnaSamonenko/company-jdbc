4c20505b2665156b776b015c3e7f2e43a40dfa83
AnnaSamonenko <annsamonenko97@gmail.com>
2018-10-17 10:34:14 +0300

    Changes in script

--- src/main/java/entrypoint/Main.java
+++ src/main/java/entrypoint/Main.java
@@ -29,7 +29,7 @@ public class Main {
         System.out.println();
         System.out.println("------ RESULT OF 2 QUERY ------");
         executeQuery2();
-        cleanTables();
+        //cleanTables();
     }
 
     //Find the PM of the project with the highest count of java developers
